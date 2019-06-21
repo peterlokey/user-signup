ca17984e736e18baa7e86ef79bb29bcd26129f8d
Peter Lokey <peter.lokey1@gmail.com>
2019-06-21 00:35:46 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, redirect, render_template
+import cgi
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True      # displays runtime errors in the browser, too
+
+@app.route('/')
+def index():
+    username_error = request.args.get('username_error')
+    return render_template('form.html', username_error=username_error)
+
+@app.route('/validate', methods=['POST'])
+def validate():
+    username = request.form['username']
+    password = request.form['password']
+    password_v = request.form['password-v']
+    email = request.form['email']
+    
+    #validate username
+    username_error = ''
+    if len(username) < 3 or len(username) > 20:
+        username_error = 'User Names must be between 3 and 20 characters'
+    if ' ' in username:
+        username_error = 'User Names may not contain spaces'
+    if not username:
+        username_error = 'Please enter a username'
+    
+    #validate password
+    password_error = ''
+    if password != password_v:
+        password_error = 'Passwords must match!'
+    if len(password) < 3 or len(password) > 20:
+        password_error = 'Password must be between 3 and 20 characters'
+    if not password:
+        password_error = 'Please enter a password'
+
+    #validate email    
+
+
+
+    if username_error or password_error:
+        return redirect('/?username_error=' + username_error + '&password_error=' + password_error)
+
+
+app.run()
